f241062963054109a7b207c5177ee70ef6c7eb57
igorbr <Igor.Bronovytskyi@playtech.com>
2018-01-22 19:46:04 +0200

    refactoring

--- js/main.js
+++ js/main.js
@@ -1,9 +1,11 @@
-var button = document.querySelector('.open button');
-var body = document.querySelector('.opening');
-button.addEventListener('click', function() {
-    if (body.style.display === '') {
-        body.style.display = 'block';
-    } else {
-        body.style.display = '';
-    }
+window.addEventListener('load', function() {
+    var button = document.querySelector('.open button');
+    var body = document.querySelector('.opening');
+    button.addEventListener('click', function() {
+        if (body.style.display === '') {
+            body.style.display = 'block';
+        } else {
+            body.style.display = '';
+        }
+    });
 });
