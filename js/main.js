1c8baa5a2a804ec9a14de7c7c97ee63951fa9ae8
igorbr <Igor.Bronovytskyi@playtech.com>
2018-01-28 15:30:57 +0200

    refactoring

--- js/main.js
+++ js/main.js
@@ -1,18 +1,20 @@
 window.addEventListener('load', function() {
     var button = document.querySelector('.open button');
     var body = document.querySelector('.opening');
+
     button.addEventListener('click', function() {
         if (body.style.display === '') {
             body.style.display = 'block';
         } else {
             body.style.display = '';
         }
-    });
 
-    button.addEventListener('click', function() {
-        var el = document.querySelectorAll('.footer');
-        if (el[0].style.bottom === '') {
-            el[0].style.bottom = '129px';
-        } else {el[0].style.bottom = ''}
+        var $footer = document.querySelector('.footer');
+        if ($footer.style.bottom === '') {
+            $footer.style.bottom = '129px';
+        } else {
+            $footer.style.bottom = '';
+        }
+
     });
 });
